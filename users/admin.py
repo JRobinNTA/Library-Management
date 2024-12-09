a557f0447a126d076e34f645e55f57e3de58f7bf
Robin John <johnrobin403@gmail.com>
2024-12-09 18:43:45 +0530

    added groups to admin page

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
-# Register your models here.
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ['username', 'email', 'membership_type', 'is_staff', 'registered_date']
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('membership_type',)}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {'fields': ('membership_type',)}),
+    )
+
+admin.site.register(CustomUser, CustomUserAdmin)
